09b38fc506083245fc2a3d64aa1fb04a9e7e61c9
ndush <mdamarisnduku@gmail.com>
2025-01-08 13:54:32 +0300

    Add HereService to fetch tourist attractions by location using HERE API

--- app/services/here_service.rb
+++ app/services/here_service.rb
@@ -1,23 +1,42 @@
+require 'httparty'
+require 'rgeo'
+require 'rgeo/geos' # Optional but recommended
+
 class HereService
   include HTTParty
   base_uri 'https://discover.search.hereapi.com/v1'
 
   def initialize(api_key)
     @api_key = api_key
+    @factory = RGeo::Geographic.spherical_factory(srid: 4326)
   end
 
-  def get_attractions_by_location(latitude, longitude)
-    response = self.class.get('/discover', query: { 
-      at: "#{latitude},#{longitude}", 
-      apiKey: @api_key, 
-      q: 'tourist attractions', 
-      size: 10  
-    })
+  def get_attractions_by_location(latitude, longitude, radius = 5000)
+    begin
+        options = {
+          query: {
+            apiKey: @api_key,
+            q: 'tourist attractions',
+            size: 10,
+            at: "#{latitude},#{longitude}" # Use 'at' parameter for location
+          }
+        }
 
-    if response.success?
-      JSON.parse(response.body)
-    else
-      { error: "Unable to fetch data", status: response.code, message: response.message }
+      Rails.logger.debug "HERE API Request: #{self.class.get('/discover', options).request.uri}"
+
+      response = self.class.get('/discover', options)
+
+      Rails.logger.debug "HERE API Response: #{response.body}"
+
+      if response.success?
+        JSON.parse(response.body)
+      else
+        Rails.logger.error "HERE API Error: #{response.code} - #{response.body}"
+        { error: "Unable to fetch data", status: response.code, message: response.message }
+      end
+    rescue StandardError => e
+      Rails.logger.error "Error during HERE API request: #{e.message}"
+      { error: "An error occurred during the request", message: e.message }
     end
   end
 end
