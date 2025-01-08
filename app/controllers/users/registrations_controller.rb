c6e3ef7e9f7a45c1ed7f674cb89e844ef7d3ea74
ndush <mdamarisnduku@gmail.com>
2025-01-08 13:50:01 +0300

    Override Devise registration to return JWT token in Users::RegistrationsController.

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,23 @@
+# app/controllers/users/registrations_controller.rb
+class Users::RegistrationsController < Devise::RegistrationsController
+   skip_before_action :authenticate_user!, only: [:create]
+  # Disable the default session handling by overriding the create action
+  def create
+    super do |resource|
+      if resource.persisted?  # Check if user was successfully created
+        token = generate_jwt(resource)  # Generate JWT token
+        render json: { user: resource, token: token }, status: :created and return
+      end
+    end
+  end
+
+  private
+
+  # Method to generate JWT token
+  def generate_jwt(user)
+    JWT.encode(
+      { user_id: user.id, exp: 24.hours.from_now.to_i },  # JWT payload
+      Rails.application.secret_key_base  # Secret key to sign JWT
+    )
+  end
+end
