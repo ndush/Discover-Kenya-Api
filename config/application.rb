418ce868c16775fb5cec2531ff21a0abe7fbd65b
ndush <mdamarisnduku@gmail.com>
2025-01-20 02:24:22 +0300

    update config

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,8 @@ module TouristAttractionsApi
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-config.autoload_paths 
+    config.autoload_paths += %W(#{config.root}/app/controllers)
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
