789d15e46a049a32e4906ee88cd1581272f82900
ndush <mdamarisnduku@gmail.com>
2025-01-25 22:24:06 +0300

    update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,21 +1,26 @@
 module Users
-class Users::RegistrationsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:create]
- def create
-    super do |resource|
-      if resource.persisted?
-        token = generate_redis_token(resource)
-        render json: { user: resource, token: token }, status: :created and return
+  class RegistrationsController < ApplicationController
+    skip_before_action :authenticate_user!, only: [:create]
+
+    def create
+      super do |resource|
+        if resource.persisted?
+          token = generate_redis_token(resource)
+          render json: { user: resource, token: token }, status: :created and return
+        end
       end
     end
-  end
-  private
-  def generate_redis_token(user)
-    token = SecureRandom.hex(20)
-    redis.setex("user:#{user.id}:session_token", 24.hours.to_i, token)
-    token
-  end
-  def redis
-    @redis ||= Redis.new
+
+    private
+
+    def generate_redis_token(user)
+      token = SecureRandom.hex(20)
+      redis.setex("user:#{user.id}:session_token", 24.hours.to_i, token)
+      token
+    end
+
+    def redis
+      @redis ||= Redis.new
+    end
   end
 end
